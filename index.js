b8f94a26a712acd67e4d359b176a5d9407da76ee
Matej Voboril <matej@voboril.org>
2017-01-07 00:24:26 -0600

    more formatting

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const maxCacheLength = process.env.LOCATION_MAX_CACHED_TIME || 60000;
  * Describes a query against the location data at http://xenogelion.com/Hidden/Relics.json
  */
 class LocationQuery {
-  
+
   /**
    * Initialize Location query object, create the initial cache of data.
    */
@@ -24,7 +24,7 @@ class LocationQuery {
   /**
    * Get the locations for a component based on the desired query.
    * @param {string} query Component query to search for the desired component with.
-   * @returns {Promise<Array<String>>}
+   * @returns {Promise<Array<string>>}
    */
   getLocationsForComponent(query) {
     return new Promise((resolve, reject) => {
@@ -41,6 +41,34 @@ class LocationQuery {
           }
           const componentHandler = new ComponentHandler(results.value);
           resolve(componentHandler.toStringList());
+        })
+        .catch((err) => {
+          resolve(defaultString);
+          reject(err);
+        });
+    });
+  }
+
+  /**
+   * Get the locations for a component based on the desired query.
+   * @param {string} query Component query to search for the desired component with.
+   * @returns {Promise<Array<Item>>}
+   */
+  getAll(query) {
+    return new Promise((resolve, reject) => {
+      const defaultString = `${md.codeMulti}Operator, there is no such item location available.${md.blockEnd}`;
+      this.cache.getData()
+        .then((dataCache) => {
+          const results = jsonQuery(`components[*component~/${query}/i]`, {
+            data: dataCache,
+            allowRegexp: true,
+          });
+          if (typeof results.value === 'undefined' || results == null) {
+            reject(new Error('No value for given query - LocationQuery#getLocationsForComponent'
+                               , 'warframe-location-query/index.js', 67));
+          }
+          const componentHandler = new ComponentHandler(results.value);
+          resolve(componentHandler.getAll());
         })
         .catch((err) => {
           resolve(defaultString);
